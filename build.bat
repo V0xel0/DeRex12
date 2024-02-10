dc608b34071399256283dec6d05bd46cc91686ad
Kacper <kluczakcg@gmail.com>
2024-02-10 22:06:49 +0100

    Start of DXC and root creation with IA layout

--- build.bat
+++ build.bat
@@ -6,10 +6,12 @@ where /q cl || (
 )
 
 set warnings=/WX /W4 /wd4201 /wd4100 /wd4189 /wd4505 /wd4701 /wd4101 /wd4324
-set includes=/I ../my_lib/ /I ../external/ /I ../external/D3D12/headers/d3dx12/ /I ../external/D3D12/headers/ 
+set includes=/I ../my_lib/ /I ../external/ /I ../external/D3D12/headers/d3dx12/ /I ../external/dxc/ /I ../external/D3D12/headers/ 
 set linkerFlags=/OUT:DeRex12.exe /INCREMENTAL:NO /OPT:REF /CGTHREADS:6 /STACK:0x100000,0x100000 
-set linkerLibs=user32.lib gdi32.lib winmm.lib dxguid.lib dxgi.lib d3d12.lib
+set linkerLibs=user32.lib gdi32.lib winmm.lib dxguid.lib dxgi.lib d3d12.lib dxcompiler.lib
 set compilerFlags=/std:c++20 /MP /arch:AVX2 /Oi /Ob3 /EHsc /fp:fast /fp:except- /nologo /GS- /Gs999999 /GR- /FC /Z7 %includes% %warnings%
+
+set dxcLib=/LIBPATH:../external/dxc/
 
 if "%~1"=="-Debug" (
 	echo [[ debug build ]]
@@ -25,5 +27,5 @@ IF NOT EXIST .\build mkdir .\build
 pushd .\build
 del *.pdb > NUL 2> NUL
 
-cl.exe %compilerFlags% ../source/Win32_x64_Platform.cpp ../source/Game.cpp ../external/D3D12MemAlloc.cpp /link %linkerFlags% %linkerLibs%
+cl.exe %compilerFlags% ../source/Win32_x64_Platform.cpp ../source/Game.cpp ../external/D3D12MemAlloc.cpp /link %dxcLib% %linkerFlags% %linkerLibs%
 popd
